7a0fc56005aae92e698be640b19d07dddfef6554
YuraHez <trisher1985@gmail.com>
2025-05-12 00:18:48 +0300

    added solutions for tasks 7,8

--- src/task-7.ts
+++ src/task-7.ts
@@ -1,10 +1,6 @@
 // Задача 7. Типізація Promise (файл task-7.ts)
 
-
-
 // Функція getMessage повертає проміс, який через затримку повертає рядок.
-
-
 
 // function getMessage() {
 //   return new Promise((resolve) => {
@@ -16,9 +12,20 @@
 
 // getMessage().then(result => console.log(result));
 
-
-
 // Завдання:
 
 // Додай до функції явну типізацію, яка вказує, що вона повертає проміс.
-// Переконайся, що значення, з яким виконається проміс, – це рядок (string).
+// Переконайся, що значення, з яким виконається проміс, - це рядок (string).
+
+function getMessage(): Promise<string> {
+    return new Promise<string>((resolve) => {
+        setTimeout(() => {
+            resolve("Hello from TS");
+        }, 1000);
+    });
+}
+
+// функція повертає Promise<string>, що означає проміс, який виконується з рядковим значенням.
+// У конструкторі Promise також додано тип-параметр <string>, щоб вказати, що resolve приймає рядок
+
+getMessage().then(result => console.log(result));
--- src/task-8.ts
+++ src/task-8.ts
@@ -1,10 +1,6 @@
 // Завдання 8. HTTP-запити (файл task-8.ts)
 
-
-
 // Функція fetchPosts робить GET-запит до API та повертає список постів.
-
-
 
 // import axios from "axios";
 
@@ -19,8 +15,6 @@
 //   console.log(posts[0].title);
 // });
 
-
-
 // Завдання:
 
 // Інсталюй бібліотеку axios командою npm i axios
@@ -29,3 +23,24 @@
 // title: рядок
 // body: рядок
 // 3. Типізуй axios.get, щоб вказати, що API повертає масив постів.
+
+
+import axios from "axios";
+
+// Інтерфейс для опису структури Post
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+
+async function fetchPosts(): Promise<Post[]> {  // Типізованр відповідь axios, вказавши, що data буде масивом Post, типізовано значення функції, яке повертаємо - "fetchPosts (Promise<Post[]>)"
+    const response = await axios.get<Post[]>(   // Типізовано виклик axios.get, вказавши, що він повертає масив постів "(axios.get<Post[]>)"
+    'https://jsonplaceholder.typicode.com/posts'
+    );
+    return response.data;
+}
+
+fetchPosts().then((posts) => {
+    console.log(posts[0].title);
+});
